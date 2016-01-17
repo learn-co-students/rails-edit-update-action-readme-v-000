5e9409e6a71d77ab0ef8d1fd178c3d989654576b
frankNowinski <nowinski.frank@gmail.com>
2016-01-16 22:28:15 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
-    @post = Post.new
   end
 
   def create
@@ -20,12 +20,16 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(title: params[:title], description: params[:description])
     redirect_to post_path(@post)
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
