1560bcfd76106735b13243df07cebc871b33312b
Mitul Mistry <mitul.mistry1@gmail.com>
2016-01-24 18:19:17 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(title: params[:title], description: params[:description])
+    #@post.update(params.require(:post)) #for form_for
     redirect_to post_path(@post)
   end
 end
