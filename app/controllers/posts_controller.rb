2bf95fec3c1243a336d4fae0c425f46910acf91a
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-05-08 17:26:46 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,11 +11,24 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+	def edit
+	  @post = Post.find(params[:id])
+	end
+ 
+	def update
+		@post = Post.find(params[:id])
+  	@post.update(title: params[:title], description: params[:description])
+  
+  	redirect_to post_path(@post)
+	end
+	
 	def create
 	  @post = Post.new
 	  @post.title = params[:title]
 	  @post.description = params[:description]
 	  @post.save
+
 	  redirect_to post_path(@post)
 	end
+
 end
