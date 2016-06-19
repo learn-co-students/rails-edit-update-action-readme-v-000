c947a4649678eb1c8853e3cf8e3d9e02e6b304cd
devinburnette <devin.burnette@gmail.com>
2016-06-18 23:05:05 -0400

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,17 +6,17 @@ describe 'navigate' do
   end
 
   it 'shows the title on the show page in a h1 tag' do
-    visit "/posts/#{@post.id}"
+    visit post_path(@post)
     expect(page).to have_css("h1", text: "My Post")
   end
 
   it 'to post pages' do
-    visit "/posts/#{@post.id}"
+    visit post_path(@post)
     expect(page.status_code).to eq(200)
   end
 
   it 'shows the description on the show page in a p tag' do
-    visit "/posts/#{@post.id}"
+    visit post_path(@post)
     expect(page).to have_css("p", text: "My post desc")
   end
 end
