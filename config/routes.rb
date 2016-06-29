8db177e20809b205e6715855e9521a508720da21
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-29 16:03:50 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create]
+  resources :posts, only: [:index, :show, :new, :create, :edit]
+  #get '/posts/:id/edit', to: 'posts#edit'
+  put 'posts/:id', to: 'posts#update'
 end
