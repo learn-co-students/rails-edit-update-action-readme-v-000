e3f742d258a6043bd9e55dc2b0573ad64d0c2367
Dom McKellar <dom.mckellar@gmail.com>
2016-07-29 14:20:47 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  
 	def index
 		@posts = Post.all
 	end
@@ -12,10 +13,24 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new
-	  @post.title = params[:title]
-	  @post.description = params[:description]
-	  @post.save
-	  redirect_to post_path(@post)
+    post = Post.create(post_params)
+	  redirect_to post_path(post)
 	end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post)
+  end
+
+  private
+
+    def post_params
+      params.permit(:title, :description)
+    end
+
 end
