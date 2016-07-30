050bcc489053d8f611afac7053f3168a6eaa3922
Jason <jasonkwong11@gmail.com>
2016-07-30 19:17:57 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create]
+resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+ #On a side note, as a shortcut you could also simply add the edit and update actions to the resources call in the routes file and that would accomplish the same goal that these two lines do.
+ 
 end
