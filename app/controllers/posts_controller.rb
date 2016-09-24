39805a63a785f05dde066c6d9003a5ddf3dea66e
smithWEBtek <brad@smithwebtek.com>
2016-09-24 16:43:59 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,6 @@ class PostsController < ApplicationController
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(title: params[:title], description: params[:description])
-	  @post.save
 	  redirect_to post_path(@post)
 	end
 
