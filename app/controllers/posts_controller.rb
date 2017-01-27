6092a19f60b6220b45aa1b8a2061fb3c41a77a87
Pia Kim <pkim2320@gmail.com>
2017-01-27 21:58:31 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,4 +18,17 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		# raise params.inspect
+		# The raise method will cause the application to pause and print out the params on an error page. You could also see the params if you called puts params.inspect; using puts would simply require you to track down the data in the Rails server log.
+
+		@post = Post.find(params[:id])
+@post.update(title: params[:title], description: params[:description])
+redirect_to post_path(@post)
+	end
 end
