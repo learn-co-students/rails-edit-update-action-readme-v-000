62a8a634381e55d8e2bae002669a4f333ac2ea54
CLackrone <charisaanne@gmail.com>
2017-07-21 10:22:19 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create]
+
+  #These two lines could be written as: 
+  #resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  put 'posts/:id', to: 'posts#update'
+
 end
