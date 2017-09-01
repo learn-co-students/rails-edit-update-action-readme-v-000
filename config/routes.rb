a6adf5628dddf645478e15f6fbd8008285f73bd8
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-01 10:48:02 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create]
+
+  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+
+  put 'posts/:id', to: 'posts#update'
+# you could also simply add the edit and update actions to 
+# the resources call in the routes file
 end
