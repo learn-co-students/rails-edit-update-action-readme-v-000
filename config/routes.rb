f4fc250531edfa95a17796033685646d02a4d8d8
Tony <github email address>
2017-10-11 14:26:48 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  # To add the edit and update routes manually:
+  # get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  # put 'posts/:id', to: 'posts#update'
+
 end
