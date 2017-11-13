1a093b1a175b4da44065eb3f03ad169cf2cd7c72
halfields <hfields697@aol.com>
2017-11-13 00:56:13 -0600

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,16 @@ class PostsController < ApplicationController
 		@posts = Post.all
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(title: params[:title], description: params[:description])
+		redirect_to post_path(@post)
+	end
+
 	def show
 		@post = Post.find(params[:id])
 	end
@@ -18,4 +28,6 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
+
+
 end
