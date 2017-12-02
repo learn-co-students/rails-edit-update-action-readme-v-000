095ca871ed9dca6fb9ecdaf2b8da080980910a3e
veloblank <veloblank@gmail.com>
2017-12-02 13:35:35 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,16 @@ class PostsController < ApplicationController
 		@posts = Post.all
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(title: params[:title], description: params[:description])
+		redirect_to post_path(@post)
+	end
+
 	def show
 		@post = Post.find(params[:id])
 	end
