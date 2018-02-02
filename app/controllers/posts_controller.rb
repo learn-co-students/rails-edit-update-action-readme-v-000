2d9a6b206cd8b3e842c83b99f675eaf2543abbff
Alex <github email address>
2018-02-02 14:49:37 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,4 +18,14 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
+	def edit
+			@post = Post.find_by(id: params[:id])
+	end
+	def update
+			 #raise params.inspect
+			 #The raise method will cause the application to pause and print out the params on an error page. You could also see the params if you called puts params.inspect; using puts would simply require you to track down the data in the Rails server lo
+			 @post = Post.find(params[:id])
+				@post.update(title: params[:title], description: params[:description])#note this update method is from Active Record and is not the update action used by rails
+				redirect_to post_path(@post)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create]
+  resources :posts, only: [:index, :show, :new, :create, :update, :edit]
+  #or inserted as
+  #     get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  #     put 'posts/:id', to: 'posts#update'  
 end
