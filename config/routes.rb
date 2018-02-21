bcd78bfaf3fad79f91b3e0cce93a2554c98a4024
Caroline Shields <caroline.shields@amkor.com>
2018-02-21 20:57:27 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  #can add :edit, :update to the resources call
   resources :posts, only: [:index, :show, :new, :create]
+  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  put 'posts/:id', to: 'posts#update'
 end
