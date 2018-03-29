73981142ddcf9d056b0be17ed6a32c6fcc389497
Chris Henry <cdhenry@gmail.com>
2018-03-29 03:01:57 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,12 @@ class PostsController < ApplicationController
 		@posts = Post.all
 	end
 
-	def show
+	def find_post
 		@post = Post.find(params[:id])
+	end
+
+	def show
+		find_post
 	end
 
 	def new
@@ -17,5 +21,15 @@ class PostsController < ApplicationController
 	  @post.description = params[:description]
 	  @post.save
 	  redirect_to post_path(@post)
+	end
+
+	def edit
+		find_post
+	end
+
+	def update
+		find_post
+		@post.update(title: params[:title], description: params[:description])
+		redirect_to post_path(@post)
 	end
 end
