d5bbe86cb1d7f92c4743a23697eff3e38009947c
AlexFriedland <friedlandaj@gmail.com>
2018-05-22 14:12:24 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  #can also just add edit and update actions to this
   resources :posts, only: [:index, :show, :new, :create]
+
+  #dynamic
+  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+
+  #edit - it's not a get, not a post...it's a PUT
+  put 'posts/:id', to: 'posts#update'
+
+  
+
 end
