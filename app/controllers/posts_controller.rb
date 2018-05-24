f45738848d847b47694399a0f5619b9322d605ef
mstmari <michael.landrum.st.marie@gmail.com>
2018-05-24 00:56:13 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,25 @@ class PostsController < ApplicationController
 		@posts = Post.all
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+
+	end
+
 	def show
 		@post = Post.find(params[:id])
 	end
 
 	def new
 		@post = Post.new
+	end
+
+
+
+	def update
+		@post = Post.find(params[:id])
+		  @post.update(title: params[:title], description: params[:description])
+		  redirect_to post_path(@post)
 	end
 
 	def create
