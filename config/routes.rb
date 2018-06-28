727e77c9f2b1d8d889bd6398aefc0ae801c5436a
Diego Yanez <diegoyanez@MacBook-Pro-4.fios-router.home>
2018-06-28 13:26:51 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create]
+
+  get '/posts/:id/edit', to: 'posts#edit', as: :edit_post
+
+  put '/posts/:id', to: 'posts#update'
 end
