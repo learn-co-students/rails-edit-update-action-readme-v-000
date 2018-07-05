ec06097c67a0075675c27e789b55fbb9400d82b4
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-07-05 16:00:23 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230232412) do
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
