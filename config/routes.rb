606ff06d330ae3023d1b087f6389bb2aebbeb514
github username <github email address>
2018-07-31 20:21:22 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create]
+  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  # we're sending data to the server, so we know it's not GET, and since we're not creating a new record it shouldn't be POST. That's right- PUT should be the HTTP verb!
+  put 'posts/:id', to: 'posts#update'
 end
