e23a953387765fef4da85b34eecc576420e57240
monksandninjas <github email address>
2018-08-16 20:11:51 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,16 @@ class PostsController < ApplicationController
 	  @post.title = params[:title]
 	  @post.description = params[:description]
 	  @post.save
-	  redirect_to post_path(@post)
+	   redirect_to post_path(@post)
+	end
+
+	def edit
+  	@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+  	@post.update(title: params[:title], description: params[:description])
+  	 redirect_to post_path(@post)
 	end
 end
