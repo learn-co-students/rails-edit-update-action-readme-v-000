afc5ca5db3250be6be5724d6fe9cf7335a3aa7da
emilywelsch <emwelsch@gmail.com>
2018-09-15 17:08:22 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,9 +20,13 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(title: params[:title], description: params[:description])
+    redirect_to post_path(@post)
   end
-  
+
 end
