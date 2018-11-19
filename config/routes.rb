a2d63f4a09d58cf1a6309f6501559674d5de7592
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-19 14:44:29 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :articles, only: %i[index show new create edit update]
+
+  # The below routes create an edit and update route that are dynamic and accept a unique :id
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch 'articles/:id', to: 'articles#update'
 end
