8993f8a1587db466700dc15d14a581b04fb91057
RonSala <ron.sala@gmail.com>
2018-12-23 03:21:51 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,6 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-        # byebug
-    # nominal
   end
 
   def new
@@ -19,8 +17,6 @@ class ArticlesController < ApplicationController
     @article.description = params[:description]
     @article.save
     redirect_to article_path(@article)
-    # byebug
-    # nominal
   end
 
   def edit
@@ -29,18 +25,7 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    @article.update(title: params[:title], description: params[:description])
+    @article.update(params.require(:article))
     redirect_to article_path(@article)
-    # byebug
-#     @article
-# #<Article id: 1, title: nil, description: nil, created_at: "2018-12-23 07:03:24", updated_at: "2018-12-23 07:03:24">
-# (byebug) params
-# {"utf8"=>"✓", "_method"=>"patch", "article"=>{"title"=>"My edit", "description"=>"My article description"}, "commit"=>"Submit Article", "controller"=>"articles", "action"=>"update", "id"=>"1"}
-# (byebug) params[:id]
-# "1"
-# (byebug) Article.all
-# #<ActiveRecord::Relation [#<Article id: 1, title: nil, description: nil, created_at: "2018-12-23 07:03:24", updated_at: "2018-12-23 07:03:24">]>
-# (byebug) article_path(@article)
-# "/articles/1"
   end  
 end
