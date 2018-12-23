bbdce4aa93f522239eaf5dc0b18ceecfcace72e5
RonSala <ron.sala@gmail.com>
2018-12-23 02:52:04 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+        # byebug
+    # nominal
   end
 
   def new
@@ -17,7 +19,28 @@ class ArticlesController < ApplicationController
     @article.description = params[:description]
     @article.save
     redirect_to article_path(@article)
+    # byebug
+    # nominal
   end
 
-  # add edit and update methods here
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], description: params[:description])
+    redirect_to article_path(@article)
+    # byebug
+#     @article
+# #<Article id: 1, title: nil, description: nil, created_at: "2018-12-23 07:03:24", updated_at: "2018-12-23 07:03:24">
+# (byebug) params
+# {"utf8"=>"✓", "_method"=>"patch", "article"=>{"title"=>"My edit", "description"=>"My article description"}, "commit"=>"Submit Article", "controller"=>"articles", "action"=>"update", "id"=>"1"}
+# (byebug) params[:id]
+# "1"
+# (byebug) Article.all
+# #<ActiveRecord::Relation [#<Article id: 1, title: nil, description: nil, created_at: "2018-12-23 07:03:24", updated_at: "2018-12-23 07:03:24">]>
+# (byebug) article_path(@article)
+# "/articles/1"
+  end  
 end
