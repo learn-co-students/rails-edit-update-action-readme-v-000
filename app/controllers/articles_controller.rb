969c15e2b95c17de9ada280d92afbbc4cdfe0f5d
HarleyHarl <harleymteden@gmail.com>
2019-01-14 12:37:37 -0800

    passing all tests

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,9 +20,13 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], description: params[:description])
+    redirect_to article_path(@article)
   end
-  
+
 end
