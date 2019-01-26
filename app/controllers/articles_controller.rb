dbbe0bcf133c4fe4855b0d1e9b02709169faaa60
Nikki A <nikki.apostolakis@gmail.com>
2019-01-26 20:14:53 +0000

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,9 +25,9 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    # raise params.inspect
+    #raise params.inspect
     @article = Article.find(params[:id])
-    @article.update(title: params[:title], description: params[:description])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
     redirect_to article_path(@article)
   end
 end
