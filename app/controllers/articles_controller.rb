fe8c639e0918c8110be5aac8eaa6efe8983c5e1c
Nikki A <nikki.apostolakis@gmail.com>
2019-01-26 19:58:02 +0000

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,4 +23,11 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
+
+  def update
+    # raise params.inspect
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], description: params[:description])
+    redirect_to article_path(@article)
+  end
 end
