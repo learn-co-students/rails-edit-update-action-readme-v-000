1d3a6f8a002c946d1cf94314aacb620b976dec0a
MargaritaKhodzka <mkhodzka@gmail.com>
2019-02-18 16:04:00 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :articles, only: %i[index show new create edit update]
+  # get 'acticles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch 'acticles/:id', to: 'articles#update'
 end
