2daa9b48cd17f8a38c4562b23b4111eb58638e13
aljimenez5 <arvida.jimenez@gmail.com>
2019-02-17 21:14:39 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :articles, only: %i[index show new create edit update]
+  # resources :articles, only: %i[index show new create edit update]
+  get 'articles', to: 'articles#index', as: :articles
+  post 'articles', to: 'articles#create'
+  get 'articles/new', to: 'articles#new', as: :new_article
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  get 'articles/:id', to: 'articles#show', as: :article
+  patch 'articles/:id', to: 'articles#update'
 end
