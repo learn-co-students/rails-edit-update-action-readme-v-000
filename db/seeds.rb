df0ef7955806507bd0ba73ce7722000787d319e4
Argonity <maurice.argoetti@gmail.com>
2019-04-18 20:54:51 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+    TestArticle = Article.create(title: "TestTitle", description: "TestDescription")
