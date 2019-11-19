b4021a66689ce02e1c37402b2a68fd7c1149b007
sdcro <sdcrouse91@gmail.com>
2019-11-19 01:13:43 -0800

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,15 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    # raise params.inspect
+    # puts params.inspect
+
+    @article = Article.find(params[:id]) 
+    # Note: The article's id is stored in params[:id], NOT params[:article][:id]
     
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    # @article.update(params[:article]) # This gets a Forbidden Attributes error.
+
+    redirect_to article_path(@article)
   end
 end
